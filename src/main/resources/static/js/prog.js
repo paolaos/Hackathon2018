b6bf47b5245e95250b77533d6023e6458ef7278e
mmlara <milton.lara.mendez@gmail.com>
2018-05-17 23:38:20 -0600

    Prog js

---
+++ src/main/resources/static/js/prog.js
@@ -0,0 +1,55 @@
+(function ($) {
+    var prog = {
+        progImageSource: "https://cdn.dribbble.com/users/722835/screenshots/4082720/bot_icon.gif",
+
+        getSuggestionImage: function () {
+            var suggestedImage = document.createElement('img');
+            suggestedImage.class = 'prog-image-suggestion';
+            suggestedImage.src = progImageSource;
+            return suggestedImage;
+        },
+
+        recommendedSolution: function (exception) {
+            var bestSolution = "Not found solution for this exception";
+            $.ajax({
+                type: 'GET',
+                url: '/exception/getSolution',
+                data: {
+                    id: exception,
+                },
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                success: function (response) {
+                    bestSolution = JSON.stringify(response);
+                },
+                error: function (error) {
+                    console.log("Not found solution for this exception");
+                }
+            });
+            return bestSolution;
+        }
+        ,
+
+        searchForAction: function () {
+            var currentLink = window.location.href
+            if (currentLink.contains("exception?id=") > 0) {
+                var suggestionWindow = document.createElement('div');
+                suggestionWindow.class = 'solution-box';
+                suggestionWindow.appendChild(this.getSuggestionImage());
+                suggestionWindow.appendChild(this.recommendedSolution($('#exception-id')));
+                $.notify(
+                    suggestionWindow,
+                    {
+                        globalPosition: 'top right',
+                    }
+                );
+            }
+
+
+        }
+    }
+    $(document).ready(function () {
+        prog.searchForAction();
+    });
+
+}(jQuery));
